958d31eb75293cee3aaf0c571734f8a5f6c8c692
abseejp <30359376+abseejp@users.noreply.github.com>
2018-04-13 08:12:19 +0100

    Add files via upload

---
+++ figma.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>figma page</title>
+</head>
+<body style="background-color: #E5E5E5">
+	<h1 style="text-align: center; font-size: 60px"> WELCOME TO FIGMA 4.0</h1>
+	<h1 style="text-align: center; font-size: 60px;"> I AM Abseejp</h1>
+	<h1 style="text-align: center; font-size: 60px;"><?php 
+		date_default_timezone_set("Africa/Lagos");
+		echo date("l")."<br>";
+		echo date("Y/m/d"). "<br>";
+		echo date("h:i:sa");
+	 ?>
+
+	 </h1>
+
+</body>
+</html>
